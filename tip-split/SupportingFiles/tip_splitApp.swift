407b152a70955f91263bead53286ef7d7054b2ec
Erick Pac <erickj.pac@gmail.com>
2022-09-06 10:03:53 -0600

    initial commit

---
+++ tip-split/SupportingFiles/tip_splitApp.swift
@@ -0,0 +1,17 @@
+//
+//  tip_splitApp.swift
+//  tip-split
+//
+//  Created by epac on 6/09/22.
+//
+
+import SwiftUI
+
+@main
+struct tip_splitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
